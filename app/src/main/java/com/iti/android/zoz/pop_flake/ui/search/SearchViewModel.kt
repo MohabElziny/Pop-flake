e8051c1c85673fefefad08b6795dd12b88190eab
Mohab El-Ziny <32043983+MohabElziny@users.noreply.github.com>
2022-08-01 18:12:22 +0200

    prepare working

--- app/src/main/java/com/iti/android/zoz/pop_flake/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/iti/android/zoz/pop_flake/ui/search/SearchViewModel.kt
@@ -1,13 +1,13 @@
-package com.iti.android.zoz.pop_flake.ui.notifications
+package com.iti.android.zoz.pop_flake.ui.search
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class SearchViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
 }
