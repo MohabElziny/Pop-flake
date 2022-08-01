98b1b05baa84fb6c97c1bea34d463063f5eab535
Mohab El-Ziny <32043983+MohabElziny@users.noreply.github.com>
2022-08-01 17:56:53 +0200

    Create Application

---
+++ app/src/main/java/com/iti/android/zoz/pop_flake/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.iti.android.zoz.pop_flake.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
